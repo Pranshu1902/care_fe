88dec7c349b501c8f9993855d3f781910ea4973b
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-03-16 18:29:54 +0530

    added template

--- src/Components/Auth/ResetPassword.tsx
+++ src/Components/Auth/ResetPassword.tsx
@@ -47,6 +47,10 @@ export const ResetPassword = (props: any) => {
     fieldValue[name] = value;
     setPassReg(0);
     setForm(fieldValue);
+
+    // validate form on every change
+    validateData();
+    console.log(errors);
   };
 
   const validateData = () => {
@@ -54,20 +58,63 @@ export const ResetPassword = (props: any) => {
     const err = Object.assign({}, errors);
     if (form.password !== form.confirm) {
       hasError = true;
-      setPassReg(1);
+      // setPassReg(1);
       err.confirm = t("password_mismatch");
+    } else {
+      err.confirm = "";
     }
 
     const regex = /^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[!@#$%^&*]).{8,}$/;
     if (!regex.test(form.password)) {
       hasError = true;
       err.password = t("invalid_password");
+    } else {
+      err.password = "";
+    }
+
+    // sub regexs
+    const digitRegex = /^(?=.*[0-9]+).{1,}$/;
+    const upperRegex = /^(?=.*[A-Z]+).{1,}$/;
+    const lowerRegex = /^(?=.*[a-z]+).{1,}$/;
+    const symbolRegex = /^(?=.*[!@#$%^&*]).{1,}$/;
+
+    if (!digitRegex.test(form.password)) {
+      hasError = true;
+      err.containsDigit = false;
+    } else {
+      err.containsDigit = true;
+    }
+    if (!upperRegex.test(form.password)) {
+      hasError = true;
+      err.containsUpperCase = false;
+    } else {
+      err.containsUpperCase = true;
+    }
+    if (!lowerRegex.test(form.password)) {
+      hasError = true;
+      err.containsLowerCase = false;
+    } else {
+      err.containsLowerCase = true;
+    }
+    if (!symbolRegex.test(form.password)) {
+      hasError = true;
+      err.containsSymbol = false;
+    } else {
+      err.containsSymbol = true;
+    }
+    if (form.password.length < 8) {
+      hasError = true;
+      err.passwordLength = false;
+    } else {
+      err.passwordLength = true;
     }
 
     Object.keys(form).forEach((key) => {
       if (!form[key]) {
         hasError = true;
         err[key] = t("field_required");
+      } else {
+        err[key] = "";
       }
     });
     if (hasError) {
@@ -140,11 +187,41 @@ export const ResetPassword = (props: any) => {
                   <ExpansionPanel>
                     <ExpansionPanelDetails>
                       <Typography className="text-red-500">
-                        <li>{t("min_password_len_8")}</li>
-                        <li>{t("req_atleast_one_digit")}</li>
-                        <li>{t("req_atleast_one_uppercase")}</li>
-                        <li>{t("req_atleast_one_lowercase")}</li>
-                        <li>{t("req_atleast_one_symbol")}</li>
+                        <li
+                          className={`${
+                            errors.passwordLength && "text-primary-500"
+                          }`}
+                        >
+                          {t("min_password_len_8")}
+                        </li>
+                        <li
+                          className={`${
+                            errors.containsDigit && "text-primary-500"
+                          }`}
+                        >
+                          {t("req_atleast_one_digit")}
+                        </li>
+                        <li
+                          className={`${
+                            errors.containsUpperCase && "text-primary-500"
+                          }`}
+                        >
+                          {t("req_atleast_one_uppercase")}
+                        </li>
+                        <li
+                          className={`${
+                            errors.containsLowerCase && "text-primary-500"
+                          }`}
+                        >
+                          {t("req_atleast_one_lowercase")}
+                        </li>
+                        <li
+                          className={`${
+                            errors.containsSymbol && "text-primary-500"
+                          }`}
+                        >
+                          {t("req_atleast_one_symbol")}
+                        </li>
                       </Typography>
                     </ExpansionPanelDetails>
                   </ExpansionPanel>
